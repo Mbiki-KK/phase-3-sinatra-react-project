e82515b6fd4f4f8e801f383a64735c604ecfe5ad
Mbiki-KK <wawerumbiki@gmail.com>
2023-06-07 15:48:21 +0300

    model files added

--- db/migrate/20230607110742_create_contacts_table.rb
+++ db/migrate/20230607110742_create_contacts_table.rb
@@ -1,10 +1,13 @@
 class CreateContactsTable < ActiveRecord::Migration[6.1]
   def change
     create_table :contacts do |t|
+      t.integer :id
       t.string :name
       t.string :email
+      t.phone :phone
       t.string :status
       t.string :location
+      t.string :relation
       t.timestamps
     end
   end
