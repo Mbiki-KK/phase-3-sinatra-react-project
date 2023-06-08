0c7793ce98dea6d6e36c83bdc07cd7c3c5b061a2
Mbiki-KK <wawerumbiki@gmail.com>
2023-06-08 23:06:13 +0300

    update button works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,18 +9,28 @@ class ApplicationController < Sinatra::Base
     contacts.to_json
   end
 
-  post "/contacts" do
-    Contact.create(
-      name: params[:name],
-      email: params[:email],
-      phone: params[:phone],
-      location: params[:location],
-      status: params[:status],
-      relation: params[:relation],
-    )
+  put '/contacts/:id' do
+    contact = Contact.find(params[:id])
+
+    if contact
+      contact.update(
+        name: params[:name],
+        email: params[:email],
+        phone: params[:phone],
+        location: params[:location],
+        status: params[:status],
+        relation: params[:relation]
+      )
+      # Optionally, you can return the updated contact as JSON
+      contact.to_json
+    else
+      status 404
+      'Contact not found'
+    end
   end
 
-  patch "contacts/:id" do
+
+  put "contacts/:id" do
     contacts=Contact.find(params[:id])
     contacts.update(
       name: params[:name],
